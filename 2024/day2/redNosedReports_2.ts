97cf000bb98bbf9a510aa4f32e2e530e8a05c420
Orlando Cuadra <orlandocuadra@Orlando-NS.local>
2024-12-03 11:46:59 -0600

    day 2-2

---
+++ 2024/day2/redNosedReports_2.ts
@@ -0,0 +1,58 @@
+import {readFileSync} from 'fs';
+import { join } from 'path';
+
+// const fileBuffer = readFileSync(join(__dirname, 'example.txt'));
+const fileBuffer = readFileSync(join(__dirname, 'challenge.txt'));
+const reports = fileBuffer.toString().trim().split('\n').reduce<number[][]>((acc, rowString) => {
+  const rowArray = rowString.split(' ').map(Number);
+  acc.push(rowArray);
+  return acc;
+}, []);
+
+let safeReports = 0;
+let ascMaxDiff = [-3, -1];
+let desMaxDiff = [1, 3];
+
+const isValidReport = (report: number[]) => {
+  const reportDifferences = report.reduce((diffs, level, i) => {
+    if(i===0){
+      return diffs;
+    }
+    const prevLevel = report[i-1];
+    
+    diffs.push(prevLevel-level)
+    
+    return diffs;
+  }, [] as number[])
+
+  const isAllAscedant = reportDifferences.every(diff => diff >= ascMaxDiff[0] && diff <= ascMaxDiff[1] )
+  const isAllDescendant = reportDifferences.every(diff => diff >= desMaxDiff[0] && diff <= desMaxDiff[1] )
+
+  if(isAllAscedant || isAllDescendant){
+    return true;
+  }
+
+  return false
+}
+
+for(const report of reports){
+  let isValid = isValidReport(report)
+  if(isValid){
+    safeReports++;
+    continue;
+  }
+  if(!isValid){
+    for (let i = 0; i < report.length; i++) {
+      let shortedReport = [...report];
+      shortedReport.splice(i, 1);
+      isValid = isValidReport(shortedReport);
+      if(isValid){
+        safeReports++;
+        break;
+      }
+    }
+  }
+}
+
+console.log(safeReports)
+
