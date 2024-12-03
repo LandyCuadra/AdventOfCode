01a36bd15c81e50d7e2e99b4196a4318f356d29a
Orlando Cuadra <orlandocuadra@Orlando-NS.local>
2024-12-03 11:23:37 -0600

    update day 2-1

--- 2024/day2/redNosedReports_1.ts
+++ 2024/day2/redNosedReports_1.ts
@@ -10,35 +10,35 @@ const reports = fileBuffer.toString().trim().split('\n').reduce<number[][]>((acc
 }, []);
 
 let safeReports = 0;
-let maxDiffer = 3;
+let ascMaxDiff = [-3, -1];
+let desMaxDiff = [1, 3];
+
+const isValidReport = (report: number[]) => {
+  const reportDifferences = report.reduce((diffs, level, i) => {
+    if(i===0){
+      return diffs;
+    }
+    const prevLevel = report[i-1];
+    
+    diffs.push(prevLevel-level)
+    
+    return diffs;
+  }, [] as number[])
+
+  const isAllAscedant = reportDifferences.every(diff => diff >= ascMaxDiff[0] && diff <= ascMaxDiff[1] )
+  const isAllDescendant = reportDifferences.every(diff => diff >= desMaxDiff[0] && diff <= desMaxDiff[1] )
+
+  if(isAllAscedant || isAllDescendant){
+    return true;
+  }
+
+  return false
+}
 
 for(const report of reports){
-  let prev=report[0];
-  let sortOrder = prev < report[1] ? 'asc' : 'desc';
-  for(let i = 1 ; i < report.length ; i++){
-    const value = report[i];
-    if(sortOrder === 'asc'){
-      if (
-        prev > value ||
-        Math.abs(prev - value) === 0 ||
-        Math.abs(prev - value) > maxDiffer
-      ) {
-        break;
-      }
-    } else {
-      if (
-        prev < value ||
-        Math.abs(prev - value) === 0 ||
-        Math.abs(prev - value) > maxDiffer
-      ) {
-        break;
-      }
-    }
-
-    if(i === report.length  - 1){
-      safeReports++;
-    }
-    prev = value;
+  const isValid = isValidReport(report)
+  if(isValid){
+    safeReports++;
   }
 }
 
